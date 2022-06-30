10c6c8416e5dab4fb17d6135334fa746a1cfe5a0
Rhokthar <molino.simone1295@gmail.com>
2022-06-30 10:02:43 +0200

    App.py started. Layout and Index fixed for Flask running app. Nav-img modified for Flask

--- project/app.py
+++ project/app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 
+# Setup App
 app = Flask(__name__)
 
+# Templates autoreload
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Index Route
 @app.route("/")
 def index():
     return render_template("index.html")
