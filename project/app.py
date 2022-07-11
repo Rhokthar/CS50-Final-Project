2f93911e5ef140bf1e2593c6851c6dd4b11ce5b3
Rhokthar <molino.simone1295@gmail.com>
2022-07-11 13:44:13 +0200

    App.py updated. skills.html created

--- project/app.py
+++ project/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -15,6 +16,29 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def index():
     return render_template("index.html")
 
+
+# About Me
 @app.route("/about-me")
 def about_me():
     return render_template("about-me.html")
+
+
+# Projects
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+# Skills
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+
+# Def Blog
+
+
+# Contacts
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
